866d89b7568a227757f8aac1fa15bd6b27d54090
suryaspandey <suryaspandey@gmail.com>
2023-09-27 00:30:10 +0530

    EditProfile page updated

--- src/Pages/EditProfile.jsx
+++ src/Pages/EditProfile.jsx
@@ -4,7 +4,35 @@ import "./editProfile.css";
 function EditProfile() {
   return (
     <>
-      <div className="editProfile-heading">EditpProfile</div>
+      <div className="editProfile-heading">Edit profile</div>
+      <div className="editProfile-img-container">
+        <ul className="editProfile-ul">
+          <li className="editProfile-li">
+            <div className="editProfile">
+              <img className="editprofile-img" src="/adult-1.png" alt="" />
+            </div>
+          </li>
+        </ul>
+        <button className="profileChange-btn">
+          <p className="change-profileImg">Change</p>
+        </button>
+        <div className="input-profileName">
+          <input
+            className="input-edit-create-name"
+            type="text"
+            minLength={1}
+            maxLength={50}
+            required
+            placeholder="Enter name"
+            value="surya pandey"
+          />
+        </div>
+
+        <div className="editProfile-save-cancel-btns">
+          <button className="editProfile-cancel-btn">Cancel</button>
+          <button className="editProfile-cancel-btn">Save Changes</button>
+        </div>
+      </div>
     </>
   );
 }
